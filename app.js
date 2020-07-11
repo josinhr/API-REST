58524a86de51484d4b773fc6a72151006d963568
José Hernández Rizo <josinhr>
2020-07-11 14:54:50 +0200

    fin

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
     methodOverride = require('method-override');
-mongose = require('mongoose');
+mongoose = require('mongoose');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -16,7 +16,27 @@ router.get('/', (req, res) => {
 
 app.use(router);
 
-app.listen(3000, () => {
-    console.log("Node server runnoing on localhost:3000");
-
+mongoose.connect('mongodb://localhost/tvshows', { useNewUrlParser: true , useUnifiedTopology: true}, (err, res) => {
+  if(err) {
+    console.log('ERROR: connecting to Database. ' + err);
+  }
+  app.listen(3000, function() {
+    console.log("Node server running on http://localhost:3000");
+  }
+  );
 });
+
+const TVShowCtrl = require('./controllers/tvshows');
+
+const tvshows = express.Router();
+
+tvshows.route('/tvshows')
+    .get(TVshowCtrl.findAllTvShows)
+    .post(TVShowCtrl.addTVShow);
+
+tvshows.route('/tvshows/:id')
+  .get(TVShowCtrl.findById)
+  .put(TVShowCtrl.updateTVShow)
+  .delete(TVShowCtrl.deleteTVShow);
+
+app.use('/api', tvshows);
